b28f21b55148c82efaaf62285c600825dc574cd0
kaushalsahu07 <kaushal.r.sahu@gmail.com>
2024-09-12 23:16:48 +0530

    day 16 :- testimonial section done

--- assets/main.js
+++ assets/main.js
@@ -2,20 +2,36 @@
 let navmenu = document.querySelector(".nav-menu");
 let navmenuBtn = document.querySelector("#bars");
 
-navmenuBtn.addEventListener("click", function(){
-    if(navmenu.style.left == "-100%" || navmenuBtn.className == "fa-solid fa-bars-staggered"){
-        navmenuBtn.className = "fa-solid fa-xmark" 
-        navmenu.style.left = "0%"
-        console.log("worked");
-    }else{
-        navmenuBtn.className = "fa-solid fa-bars-staggered"
-        navmenu.style.left = "-100%"
-    }
-}) 
+navmenuBtn.addEventListener("click", function () {
+  if (
+    navmenu.style.left == "-100%" ||
+    navmenuBtn.className == "fa-solid fa-bars-staggered"
+  ) {
+    navmenuBtn.className = "fa-solid fa-xmark";
+    navmenu.style.left = "0%";
+    console.log("worked");
+  } else {
+    navmenuBtn.className = "fa-solid fa-bars-staggered";
+    navmenu.style.left = "-100%";
+  }
+});
+
 // scroll up
 function scrollUp() {
-	const scrollUp = document.getElementById("scroll-up");
-	if (this.scrollY >= 400) scrollUp.classList.add("show-scroll");
-	else scrollUp.classList.remove("show-scroll");
+  const scrollUp = document.getElementById("scroll-up");
+  if (this.scrollY >= 400) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
 }
 window.addEventListener("scroll", scrollUp);
+
+//swiper
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  loop: true,
+  speed: 600,
+  autoplay: {
+    delay: 5000,
+  },
+});
